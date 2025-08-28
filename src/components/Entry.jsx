1dda69df3b76a75df1701cb2f38c74ddc7c1d4d3
Coding <codingmaniac227@gmail.com>
2025-08-28 04:01:25 -0400

    chore(): imported files from my secondary repo

---
+++ src/components/Entry.jsx
@@ -0,0 +1,87 @@
+
+
+export default function Entry() {
+    return (
+        <>
+            <main>
+                <article>
+                    <img src="/src/assets/japan-img.png" className='location-img' />
+                    <section className='mt-fuji-section'>
+                        
+                        <section className='mt-fuji-section-header'>
+                            <img src="/src/assets/location-icon.svg" className='icon-img' />
+                            <span>JAPAN</span>
+                            <span>
+                                <a href='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu' className='google-maps-link'>View on Google Maps</a>
+                            </span>
+                        </section>
+                        
+                        <section className='mt-fuji-section-title'>
+                            <h2>Mount Fuji</h2>
+                        </section>
+                        
+                        <section className='mt-fuji-section-body'>
+                            <h3>12 Jan, 2023 - 24 Jan, 2023</h3>
+                            <p>
+                                Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                            </p>
+                        </section>
+                        
+                    </section>
+                </article>
+                
+                <article>
+                    <img src='/src/assets/australia-img.png' className='location-img' />
+                    <section className='sydney-opera-section'>
+                        
+                        <section className='sydney-opera-section-header'>
+                            <img src="/src/assets/location-icon.svg" className='icon-img' />
+                            <span>AUSTRALIA</span>
+                            <span>
+                                <a href='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu' className='google-maps-link'>View on Google Maps</a>
+                            </span>
+                        </section>
+                        
+                        <section className='sydney-opera-section-title'>
+                            <h2>Sydney Opera House</h2>
+                        </section>
+                        
+                        <section className='sydney-opera-section-body'>
+                            <h3>01 Oct, 2021 – 18 Nov, 2021</h3>
+                            <p>
+                                The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century’s most famous and distinctive buildings.
+                            </p>
+                        </section>
+                        
+                    </section>
+                </article>
+                
+                <article>
+                    <img src='/src/assets/australia-img.png' className='location-img' />
+                    <section className='geirangerfjord-section'>
+                        
+                        <section className='geirangerfjord-section-header'>
+                            <img src="/src/assets/location-icon.svg" className='icon-img' />
+                            <span>NORWAY</span>
+                            <span>
+                                <a href='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu' className='google-maps-link'>View on Google Maps</a>
+                            </span>
+                        </section>
+                        
+                        <section className='geirangerfjord-section-title'>
+                            <h2>Geirangerfjord</h2>
+                        </section>
+                        
+                        <section className='geirangerfjord-section-body'>
+                            <h3>27 May, 2021 – 8 Jun, 2021</h3>
+                            <p>
+                                The Geiranger Fjord is a fjord in the Sunnmore region of More og Romsdal county, Norway. It is located entirely in the Stranda Municipality and is known for its dramatic cliffs, waterfalls, and scenic landscapes.
+                            </p>
+                        </section>
+                        
+                    </section>
+                </article>
+            </main>
+        </>
+    )
+}
