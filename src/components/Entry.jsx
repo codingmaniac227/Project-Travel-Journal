1c130de90f7de826a243b112ca92961a938a6c08
Coding <codingmaniac227@gmail.com>
2025-08-28 06:10:26 -0400

    deleted

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -5,11 +5,11 @@ export default function Entry() {
         <>
             <main>
                 <article>
-                    <img src="/src/assets/japan-img.png" className='location-img' />
+                    <img src="/images/japan-img.png" className='location-img' />
                     <section className='mt-fuji-section'>
                         
                         <section className='mt-fuji-section-header'>
-                            <img src="/src/assets/location-icon.svg" className='icon-img' />
+                            <img src="/images/location-icon.svg" className='icon-img' />
                             <span>JAPAN</span>
                             <span>
                                 <a href='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu' className='google-maps-link'>View on Google Maps</a>
@@ -31,11 +31,11 @@ export default function Entry() {
                 </article>
                 
                 <article>
-                    <img src='/src/assets/australia-img.png' className='location-img' />
+                    <img src='/images/australia-img.png' className='location-img' />
                     <section className='sydney-opera-section'>
                         
                         <section className='sydney-opera-section-header'>
-                            <img src="/src/assets/location-icon.svg" className='icon-img' />
+                            <img src="/images/location-icon.svg" className='icon-img' />
                             <span>AUSTRALIA</span>
                             <span>
                                 <a href='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu' className='google-maps-link'>View on Google Maps</a>
@@ -57,11 +57,11 @@ export default function Entry() {
                 </article>
                 
                 <article>
-                    <img src='/src/assets/australia-img.png' className='location-img' />
+                    <img src='/images/australia-img.png' className='location-img' />
                     <section className='geirangerfjord-section'>
                         
                         <section className='geirangerfjord-section-header'>
-                            <img src="/src/assets/location-icon.svg" className='icon-img' />
+                            <img src="/images/location-icon.svg" className='icon-img' />
                             <span>NORWAY</span>
                             <span>
                                 <a href='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu' className='google-maps-link'>View on Google Maps</a>
